baeb467dc843961abdef4b41a55917d15f2c00bf
Sven Duve <svenduve@gmail.com>
2018-09-16 15:31:50 +0200

    module export added

--- src/StatStuff.jl
+++ src/StatStuff.jl
@@ -2,4 +2,19 @@ module StatStuff
 
 # package code goes here
 
+export
+
+    meanSD,
+    medianSD,
+    varianceSD,
+    standardDeviationSD
+
+
+
+include("meanSD.jl")
+include("medianSD.jl")
+include("varianceSD.jl")
+include("standardDeviationSD.jl")
+
+
 end # module
